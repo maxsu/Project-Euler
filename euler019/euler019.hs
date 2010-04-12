d95372d8c4425f24fc85d2c1e045c0fef64b4a78
Max Suica <max.suica@gmail.com>
2010-04-12 09:09:18 -0400

    Solved problem 19.

---
+++ euler019/euler019.hs
@@ -0,0 +1,18 @@
+﻿---------------------------------------------
+-- Max Suica, 04-12-2010. max.suica@gmail.com
+
+import Data.Time
+
+a = fromGregorian 1901 1 1
+
+b = fromGregorian 2000 12 31
+
+firsts = filter ((\(_,_,a) -> a == 1).toGregorian) [a..b]
+
+firstSundays = filter (\x -> toModifiedJulianDay x `mod` 7 == 4) firsts
+
+euler19 = length firstSundays
+
+main :: IO ()
+main = print euler19
+
