8b5daa94ce605bd82ea425bb190cef3115563ee9
Max Suica <max.suica@gmail.com>
2010-04-12 15:12:25 -0400

    Solved problem21

---
+++ euler021/euler021.hs
@@ -0,0 +1,27 @@
+﻿---------------------------------------------
+-- Max Suica, 04-12-2010. max.suica@gmail.com
+
+import Euler
+import Control.Monad (ap)
+import Control.Monad.Instances
+
+
+-- let d(n) be defined as the sum of proper divisors of n
+-- note: d(n) = \prod \frac{p_k^{n_k + 1} - 1}{p_k - 1}
+
+multiPowSet [] = [[]]
+multiPowSet ((x,n):xs) =
+  ap (++) (flip map [1..n] . flip ((:) . (,) x) =<<) (multiPowSet xs)
+
+d :: Integer -> Integer
+d n = (+(- n)) . sum . map prod' . multiPowSet $ factorCount n
+ where
+  prod' = product . map (\(b,p) -> b^p)
+
+amicable n = (d.d) n == n && n /= d n
+
+euler21 = sum . filter amicable $ [2..10000]
+
+main :: IO ()
+main = print euler21
+
