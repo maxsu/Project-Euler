68e449c287c241fa00724d9263282701712ba523
Max Suica <max.suica@gmail.com>
2010-04-12 09:08:31 -0400

    Added solution to 15

---
+++ euler015/euler015.hs
@@ -0,0 +1,12 @@
+﻿---------------------------------------------
+-- Max Suica, 04-12-2010. max.suica@gmail.com
+
+fac = product . enumFromTo 1
+
+paths n = fac (2*n) `div` (2 * fac n)
+
+euler15 = paths 20
+
+main :  IO ()
+main = print euler15
+
