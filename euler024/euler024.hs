8fd2f978b1393f8af458bdf104a3753e34399c15
Max Suica <max.suica@gmail.com>
2010-04-14 13:13:21 -0400

    Solved problem 24

---
+++ euler024/euler024.hs
@@ -0,0 +1,12 @@
+﻿---------------------------------------------
+-- Max Suica, 04-14-2010. max.suica@gmail.com
+
+import Data.List (permutations, sort)
+
+nthPermutation n = (!!(n-1)) . sort . permutations $ "0123456789"
+
+euler24 = nthPermutation 1000000
+
+main :: IO ()
+main = print euler24
+
